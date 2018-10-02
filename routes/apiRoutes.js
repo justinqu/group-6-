4d19b5aaf2ef2fcf9a73283165abcb4ce0fcfb36
Justin Quinonez <justinqnns@gmail.com>
2018-10-02 16:51:53 -0400

    captures location

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,23 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all markers
-  app.get("/api/marker", function(req, res) {
-    db.markers.findAll({}).then(function(dbmarkers) {
+  app.get("/api/markers", function (req, res) {
+    db.markers.findAll({}).then(function (dbmarkers) {
       res.json(dbmarkers);
     });
   });
 
   // Create a new example
-  app.post("/api/marker", function(req, res) {
-    db.markers.create(req.body).then(function(dbmarkers) {
+  app.post("/api/markers", function (req, res) {
+    db.markers.create(req.body).then(function (dbmarkers) {
       res.json(dbmarkers);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/marker/:id", function(req, res) {
-    db.markers.destroy({ where: { id: req.params.id } }).then(function(dbmarkers) {
+  app.delete("/api/markers/:id", function (req, res) {
+    db.markers.destroy({ where: { id: req.params.id } }).then(function (dbmarkers) {
       res.json(dbmarkers);
     });
   });
